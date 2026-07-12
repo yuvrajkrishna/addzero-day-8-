14304de0cb0e4ba492848da62a5b08b74883abb5
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-07-12 08:44:55 +0530

    five optimized

---
+++ five_1stoptimized.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+
+    int n = 5;
+    int sum = 0;
+
+    for(int i = 1; i <= n; i++) {
+
+        if(i % 2 == 0)
+            sum -= i;
+        else
+            sum += i;
+    }
+
+    printf("%d", sum);
+
+    return 0;
+}
